12f1a2ecde6c14a39eb20ebf1a0bb3e68f6f0b1c
Matt <haydude@gmail.com>
2016-01-12 00:56:20 -0600

    week 2 python homework

---
+++ WebDoer/WebDoer/Doer.vb
@@ -0,0 +1,8 @@
+﻿Module Doer
+
+    Sub Main()
+        System.Diagnostics.Process.Start("C:\Program Files (x86)\Google\Chrome\Application\chrome.exe", "canvas.northwestern.edu")
+
+    End Sub
+
+End Module
